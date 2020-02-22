70bb72b62aa160ebd7e429a1d1f4cf998cfc297e
ciszkin <ciszka.v@gmail.com>
2020-02-22 19:48:07 +0300

    The first commit of Homework #1

--- app/src/main/java/by/tms/androidtestapp/MainActivity.kt
+++ app/src/main/java/by/tms/androidtestapp/MainActivity.kt
@@ -1,12 +1,23 @@
 package by.tms.androidtestapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonGoToHomeWork1 = button_homework1
+        buttonGoToHomeWork1.setOnClickListener{ goToHomeWork(buttonGoToHomeWork1) }
+    }
+
+    private fun goToHomeWork(view: View) {
+        val intent = Intent(this, HomeWorkOneActivity::class.java)
+        startActivity(intent)
     }
 }
