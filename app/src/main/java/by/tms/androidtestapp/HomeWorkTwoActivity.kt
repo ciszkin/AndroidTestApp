edc4596a8b6cd028a4c1b7764588a5a4f55b0ca1
ciszkin <ciszka.v@gmail.com>
2020-02-27 13:32:03 +0300

    The activity for HomeWork #2 added. The layout for it is combined of other layouts in separate .xml files.

--- app/src/main/java/by/tms/androidtestapp/HomeWorkTwoActivity.kt
+++ app/src/main/java/by/tms/androidtestapp/HomeWorkTwoActivity.kt
@@ -3,10 +3,10 @@ package by.tms.androidtestapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class HomeWorkOneActivity : AppCompatActivity() {
+class HomeWorkTwoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home_work_one_layout)
+        setContentView(R.layout.home_work_two_layout)
     }
 }
--- app/src/main/java/by/tms/androidtestapp/MainActivity.kt
+++ app/src/main/java/by/tms/androidtestapp/MainActivity.kt
@@ -3,18 +3,22 @@ package by.tms.androidtestapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_layout.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_layout)
 
         val buttonGoToHomeWork1 = button_homework1
         buttonGoToHomeWork1.setOnClickListener{
             startActivity(Intent(this, HomeWorkOneActivity::class.java))
+        }
+
+        val buttonGoToHomeWork2 = button_homework2
+        buttonGoToHomeWork2.setOnClickListener {
+            startActivity(Intent(this, HomeWorkTwoActivity::class.java))
         }
     }
 }
